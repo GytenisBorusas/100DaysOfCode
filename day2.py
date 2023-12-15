986c72357cc5cc0cb51fba2f18efc231eb5bb7a6
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-12-15 15:08:28 -0700

    done with day 2

---
+++ day2.py
@@ -0,0 +1,16 @@
+
+
+
+print("Welcome to the tip calculator.")
+
+total_bill = input("What was the total bill? $")
+
+tip = input("What percentage tip would you like to give? 10, 12, or 15? ")
+
+group_count = input("How many people to split the bill? ")
+
+tip = int(tip) / 100 + 1
+
+bill_for_each_member = float(total_bill) * float(tip) / float(group_count)
+
+print(f"Each person should pay: {bill_for_each_member:.2f}")
