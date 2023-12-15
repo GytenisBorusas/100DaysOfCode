442f41e5a9dc2c0ffee017a04ebf144b238ea8e8
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-12-15 13:01:12 -0700

    done with Day 1 fully

--- day1.py
+++ day1.py
@@ -1,6 +1,6 @@
 print("Welcome to the Band Name Generator.")
 
-city = input("Where did you grow up? ")
-pet_name = input("What is the name of your pet? ")
+city = input("Where did you grow up? \n")
+pet_name = input("What is the name of your pet? \n")
 
-print(f'{city} {pet_name}')
+print(f'Your band name could be "{city} {pet_name}"')
