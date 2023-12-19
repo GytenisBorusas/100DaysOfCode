f06a62e97c8570da69a8394af38d2f8009d54eea
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-12-19 12:53:07 -0700

    done with 4th class exercise

--- day5.py
+++ day5.py
@@ -60,5 +60,14 @@
 
 # Write your code here 👇
 
-
+for number in range(1, 101):
+    if number % 3 == 0:
+        if number % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
 
