fadf0f65c8dfcedb1075ab87ec5ddeac6e4fcc90
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-12-21 12:41:30 -0700

    done with Hangman game/day7

--- day7.py
+++ day7.py
@@ -1,34 +1,73 @@
 import random
+from hangman_art import logo, stages 
+from hangman_words import word_list
 
-
-#Step 1 
-
-word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
+print(logo)
 print(chosen_word)
-
 
 display = []
 
 #creates empty list for the lenght of the random word with "_" for each letter
 for _ in chosen_word:
     display += "_"
-print(display)
 
+guessed_letter_list = []
+lives = 6
 
-guess = input("Guess a letter: ").lower()
-
-
-position = 0
-#checks if the guest letter is in the word
-for letter in chosen_word:
-    if letter == guess:
-        display[position] = letter
-        position += 1
+while ("_" in display) and (lives > 0):
+    print(stages[lives])
+    print(f"{' '.join(display)}\n")
+    print(f"You have {lives} lives left.")
+    if len(guessed_letter_list) > 0:
+        print(f"You have already tried [{' '.join(guessed_letter_list)}] letter(s).\n")
+    guess = input("Guess a letter: ").lower()
+       
+    if guess in guessed_letter_list:
+        print(f'You have already guessed "{guess}" before. Try a different letter.')
     else:
-        position += 1
+        guessed_letter_list.append(guess)
+        
+
+        life_lost_flag = True
+        position = 0
+        
+        
+        #checks if the guest letter is in the word
+        for letter in chosen_word:
+            if letter == guess:
+                display[position] = letter
+                position += 1
+                life_lost_flag = False
+            else:
+                position += 1
+                
+        
+        if life_lost_flag == True:
+            print(f'Letter {guess} is not in the word.')
+            lives -= 1
+            
+        
+if lives == 0:
+    print(stages[0])
+    print("You are deaaaad!")
+else:
+    print(" ")
+    print("***************************")
+    print("You just WOOOOON!")
+    print("***************************")
+            
+    
+    
+    
+    
+     
+    
 
 
 
-print(display)
+ #TODO-3: - print the ASCII art from 'stages' that corresponds to the current 
+ # umber of 'lives' the user has remaining.
+
+
