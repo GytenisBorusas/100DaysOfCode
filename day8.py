004f35e7cc93a4603665c41e824abd8b0c421a30
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-12-22 15:01:34 -0700

    pre-restructuring code - day8

--- day8.py
+++ day8.py
@@ -52,43 +52,48 @@
 # -------------- Homework exercise: --------------
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
-            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 
+            'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 
+            't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text, shift):
-    encoded_word = []
-    for char in text:
-        letter_index = alphabet.index(char)
-        shifted_index = letter_index + shift
-        encoded_word.append(alphabet[shifted_index])
+def encrypt(plain_text, shift_amount):
+    cipher_text = []
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        cipher_text.append(alphabet[new_position])
         
+    joined_encoded_word = ''.join(cipher_text)
+    print(f'The encoded text is "{joined_encoded_word}"')
+
+def decrypt(cipher_text, shift_amount):
+    deciphered_text = []
+    reversed_alphabet = alphabet[::-1]
+    for letter in cipher_text:
+        position = reversed_alphabet.index(letter)
+        new_position = position + shift_amount
+        deciphered_text.append(reversed_alphabet[new_position])
         
-    joined_encoded_word = ''.join(encoded_word)
-    return joined_encoded_word
+    joined_decrypted_word = ''.join(deciphered_text)
+    print(f'The decoded text is "{joined_decrypted_word}"')
 
-        
-        
-        
-encripted_word = encrypt(text=text, shift=shift)
-print(encripted_word)
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in 
-    # the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to 
-# test the code and encrypt a message. 
+if direction == "encode":
+    encripted_word = encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decripted_word = decrypt(cipher_text=text, shift_amount=shift)
+else:
+    print("You can't type...")
 
 
+
+
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message 
+# by checking the 'direction' variable. Then call the correct function 
+# based on that 'drection' variable. You should be able to test the 
+# code to encrypt *AND* decrypt a message.
