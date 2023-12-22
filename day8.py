9db520116fb51577cd54ce15e53353ccf0142674
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-12-22 10:56:13 -0700

    done with class exercise of day 8

--- day8.py
+++ day8.py
@@ -23,3 +23,34 @@
 # -------------- Second class exercise: --------------
 
 
+# # Write your code below this line 👇
+# def prime_checker(number):
+#     its_prime_number = False
+#     placeholder = 0
+#     for numbers_to_check in range(2, number):
+#         if number % numbers_to_check > 0:
+#             # print(f"It's not a prime number. (Checked {number}/{numbers_to_check})")
+#             placeholder = 1
+#         else:
+#             # print(f"It's a prime number.(Checked {number}/{numbers_to_check})")
+#             its_prime_number = True
+
+#     if its_prime_number == True:
+#         print(f"It's not a prime number.")
+#     else:
+#         print(f"It's a prime number.")
+
+
+# # Write your code above this line 👆
+    
+# #Do NOT change any of the code below👇
+# n = int(input()) # Check this number
+# prime_checker(number=n)
+
+
+
+# -------------- Second class exercise: --------------
+
+
+
+
