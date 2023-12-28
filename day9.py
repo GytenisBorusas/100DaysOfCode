d3ee94b0d3e309d5c100ffd577ec925877aae53a
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-12-28 12:21:51 -0700

    Start of the day 9 classroom works

---
+++ day9.py
@@ -0,0 +1,55 @@
+# -------------- First class exercise: --------------
+
+# programming_dictionary = {
+#     "Bug": "An error in a program that prevents the program from running as expected.", 
+#     "Function": "A piece of code that you can easily call over and over again.",
+#     }
+
+# #Retrieving item from dictionary
+# # print(programming_dictionary["Function"])
+
+# #adding new items to dictionary
+# programming_dictionary["Loop"] = "The action of doing something over and over again"
+# print(programming_dictionary)
+
+# #crete an empty dictionary.
+# empty_dictionary = {}
+# print(empty_dictionary)
+
+
+# # #Wipe an existing dictionary
+# # programming_dictionary = {}
+# # print(programming_dictionary)
+
+# #edit an item a dictionary
+# programming_dictionary["Bug"] = "A month in your computer"
+# print(programming_dictionary)
+
+# #loop through a dictionary
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
+    
+    
+    
+    
+# -------------- Second class exercise: --------------
+    
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+# TODO-1: Create an empty dictionary called student_grades.
+
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
+
+
