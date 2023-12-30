38b99becff09b0c7c212905650648aabfe8df4b4
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-12-30 10:13:51 -0700

    done with day10

--- day10.py
+++ day10.py
@@ -11,33 +11,85 @@
 # -------------- First class work exercise --------------
 
 
-def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-                print("Leap year")
-            else:
-                return False
-                print("Not leap year")
-        else:
-            return True
-            print("Leap year")
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 return True
+#                 print("Leap year")
+#             else:
+#                 return False
+#                 print("Not leap year")
+#         else:
+#             return True
+#             print("Leap year")
+#     else:
+#         return False
+#         print("Not leap year")
+  
+# # TODO: Add more code here 👇
+# def days_in_month(year, month):
+#     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+#     if is_leap == False:
+#         return month_days[month-1]
+#     else:
+#         if month == int(2):
+#             return int(29)
+#         else:
+#             return month_days[month-1]
+  
+# #🚨 Do NOT change any of the code below 
+# year = int(input()) # Enter a year
+# month = int(input()) # Enter a month
+# days = days_in_month(year=year, month=month)
+# print(days)
+
+
+
+# -------------- Home class work exercise --------------
+
+from replit import clear
+from art import logo
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  print(logo)
+
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
     else:
-        return False
-        print("Not leap year")
-  
-# TODO: Add more code here 👇
-def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+      should_continue = False
+      clear()
+      calculator()
 
-  
-#🚨 Do NOT change any of the code below 
-year = int(input()) # Enter a year
-month = int(input()) # Enter a month
-days = days_in_month(year, month)
-print(days)
-
-
-
-
+calculator()
