f394c035b1222d55cd052116bd2bf5fc94fc5594
Gytenis Borusas <gytenis.borusas@gmail.com>
2024-01-02 13:25:47 -0700

    done with day12 project

--- day12.py
+++ day12.py
@@ -1,10 +1,70 @@
-################### Scope ####################
+# ################### Scope ####################
 
-enemies = 1
+# enemies = 1
 
-def increase_enemies():
-  enemies = 2
-  print(f"enemies inside function: {enemies}")
+# def increase_enemies():
+#   enemies = 2
+#   print(f"enemies inside function: {enemies}")
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+
+# # local Scope
+
+
+
+
+# ################### HW project ####################
+
+import random
+
+
+def random_number():
+    number = 0
+    numbers_list = []
+    for _ in range(100):
+        number += 1
+        numbers_list.append(number)
+    random_number = random.choice(numbers_list)
+    return random_number
+
+def intro():
+    attempts = 0
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100")
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+    if difficulty == 'easy':
+        attempts = 10
+    elif difficulty == 'hard':
+        attempts = 5
+    else:
+        print("You have made a typing mistake...")
+    return attempts
+    
+
+def game(guesses_left, number):
+    while guesses_left > 0:
+        print(f"You have {guesses_left} attepts remaining to guess the number.")
+        guess = int(input("Make a guess. "))
+        if guess > number:
+            print("Too high.")
+            guesses_left -= 1
+        elif guess < number:
+            print("Too low.")
+            guesses_left -= 1
+        else:
+            print("You have WOOOOOOOON!")
+            return
+    print("You have lost the game.")
+
+remaining_guesses = intro()
+guessing_target_number = random_number()
+game(guesses_left=remaining_guesses, number=guessing_target_number)
+
+
+
+
+
+
