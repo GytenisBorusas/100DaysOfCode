b4a4fa863909c6475fd37e0e631063081afeb908
Gytenis Borusas <gytenis.borusas@gmail.com>
2024-01-07 12:46:28 -0700

    start of day16 hw

--- day16.py
+++ day16.py
@@ -1,33 +1,48 @@
 
 
 
-# from turtle import Turtle, Screen
+# # from turtle import Turtle, Screen
 
 
 
 
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("coral", "green")
+# # timmy = Turtle()
+# # print(timmy)
+# # timmy.shape("turtle")
+# # timmy.color("coral", "green")
 
-# print(timmy.position())
-# timmy.forward(100)
-# print(timmy.position())
-
-
-
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+# # print(timmy.position())
+# # timmy.forward(100)
+# # print(timmy.position())
 
 
 
+# # my_screen = Screen()
+# # print(my_screen.canvheight)
+# # my_screen.exitonclick()
 
 
 
+# from prettytable import PrettyTable
+
+# table = PrettyTable()
+
+# table.field_names = ["Pokemon Name", "Type"]
+# table.add_row(["Pikachu", "Electric"])
+# table.add_row(["Squirtle", "Water"])
+# table.add_row(["Charmander", "Fire"])
+# table.align = "l"
 
 
+
+# print(table)
+
+
+# ----------------- Homework ------------------------
+
+from day16_menu import Menu, MenuItem
+from day16_coffee_maker import CoffeeMaker
+from day16_money_machine import MoneyMachine
 
 
 
