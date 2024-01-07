7ca0bd7da19850d2035e6f61e3944bd426d926b5
Gytenis Borusas <gytenis.borusas@gmail.com>
2024-01-06 17:31:06 -0700

    start of day16

---
+++ day16.py
@@ -0,0 +1,37 @@
+
+
+
+# from turtle import Turtle, Screen
+
+
+
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral", "green")
+
+# print(timmy.position())
+# timmy.forward(100)
+# print(timmy.position())
+
+
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
